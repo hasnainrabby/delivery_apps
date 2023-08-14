72821ca58e09616c77a1077bac0e6c4ffed9504e
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2023-08-14 21:19:53 +0600

    Custom text Field

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+
+  final TextEditingController? textController;
+  final IconData? iconData;
+  final String? hintText;
+  bool? isObscure = true;
+  bool? enable = true;
+  CustomTextField({super.key,
+    this.textController,
+    this.iconData,
+    this.hintText,
+    this.isObscure,
+    this.enable
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(5.0))
+      ),
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.all(8),
+      child: TextFormField(
+        enabled: enable,
+        controller: textController,
+        obscureText: isObscure!,
+        cursorColor: Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixIcon:Icon( iconData,
+          color: Colors.green,),
+          focusColor: Theme.of(context).primaryColor,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
