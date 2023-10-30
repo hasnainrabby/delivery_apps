fe58d9741ea1dc6809f77911cba11a67e3ae236f
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2023-10-30 18:57:58 +0600

    Error_Dialog Widget

---
+++ lib/widgets/error_dialog.dart
@@ -0,0 +1,26 @@
+import'package:flutter/material.dart';
+
+
+class ErrorDialog extends StatelessWidget
+{
+  final String? message;
+
+   ErrorDialog({this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      key: key,
+      content: Text(message!),
+      actions: [
+        ElevatedButton(onPressed: (){
+          Navigator.pop(context);
+        },style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red,
+        ), child: const Center(
+          child: Text("Ok"),
+        ),)
+      ],
+    );
+  }
+}
